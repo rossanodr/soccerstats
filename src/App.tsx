3bb72c16b903af61d600c54b9c89a5fc7d9249b6
rossanodr <rvdalarosa@hotmail.com>
2022-12-24 20:09:58 -0300

    as

--- src/App.tsx
+++ src/App.tsx
@@ -6,39 +6,37 @@ import { dataStore } from "./data";
 
 const App: React.FC = () => {
   useEffect(() => {
-    api
-      .get("sport/football/categories")
-      .then((response) => {
+    api.get('sport/football/categories')
+    .then(response => {
         // store data in dataStore
         dataStore.data = response.data;
 
         // make additional API calls
-        api
-          .get("category/13/unique-tournaments")
-          .then((response) => {
-            // store data in dataStore
-            dataStore.data = [...dataStore.data, ...response.data];
-
-            // make additional API calls
-            api
-              .get("unique-tournament/325/season/40557/standings/total")
-              .then((response) => {
+        api.get('category/13/unique-tournaments')
+            .then(response => {
                 // store data in dataStore
                 dataStore.data = [...dataStore.data, ...response.data];
 
-                // call writeToCSV to write data to CSV file  writeToCSV(dataStore);
-              })
-              .catch((error) => {
-                // handle error
-              });
-          })
-          .catch((error) => {
-            // handle error
-          });
-      })
-      .catch((error) => {
-        // handle error
-      });
+                // make additional API calls
+                api.get('unique-tournament/325/season/40557/standings/total')
+                    .then(response => {
+                        // store data in dataStore
+                        dataStore.data = [...dataStore.data, ...response.data];
+
+                        // call writeToCSV to write data to CSV file  writeToCSV(dataStore);
+})
+.catch(error => {
+    // handle error
+});
+})
+.catch(error => {
+// handle error
+});
+})
+.catch(error => {
+// handle error
+});
+}, []);
   }, []);
 
   writeToCSV(dataStore);
