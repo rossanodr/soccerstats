b1b23fd4bd2512975c6cca7dd4c3d76cec1c0e87
rossanodr <rvdalarosa@hotmail.com>
2022-12-24 19:57:38 -0300

    api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,35 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'https://api.sofascore.com/api/v1/'
-});
+const BASE_URL = 'https://api.sofascore.com/api/v1';
 
-export default api;
+export const fetchTournaments = async (): Promise<Tournament[]> => {
+  try {
+    const response = await axios.get(`${BASE_URL}/sport/football/categories`);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
+
+export const fetchTeams = async (categoryId: number): Promise<Team[]> => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/category/${categoryId}/unique-tournaments`
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
+
+export const fetchTeamInfo = async (teamId: number): Promise<TeamInfo> => {
+  try {
+    const response = await axios.get(`${BASE_URL}/team/${teamId}`);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+};
