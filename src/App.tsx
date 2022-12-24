007462a6a28ab1f607935424dee5a664971ebd03
rossanodr <rvdalarosa@hotmail.com>
2022-12-24 20:52:37 -0300

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,30 @@
-import './App.css';
-
 import React from 'react';
 
 import { cleanData, manipulateData, parseData } from './utilities';
-import {
-  getCategories,
-  getPlayerAttributeOverviews,
-  getPlayerCharacteristics,
-  getPlayerInfo,
-  getPlayerLastYearSummary,
-  getPlayerNationalTeamStatistics,
-  getPlayerPenaltyHistory,
-  getPlayerStatisticsSeasons,
-  getPlayerUniqueTournamentHeatmap,
-  getPlayerUniqueTournamentStatistics,
-  getTeamInfo,
-  getTeamPlayers,
-  getTeamStanding,
-  getTeamStandingsSeasons,
-  getTeamTransfers,
-  getTournaments
-} from './api'
+import { fetchData, writeToCSV } from './api';
 
-function App() {
-  const [categories, setCategories] = React.useState([]);
-  const [tournaments, setTournaments] = React.useState([]);
-  const [teamStats, setTeamStats] = React.useState([]);
-  const [playerStats, setPlayerStats] = React.useState([]);
-  const [transfers, setTransfers] = React.useState([]);
-  const [heatMap, setHeatMap] = React.useState([]);
-  const [penaltyHistory, setPenaltyHistory] = React.useState([]);
-  const [seasonStats, setSeasonStats] = React.useState();
-  const [nationalTeamStats, setNationalTeamStats] = React.useState([]);
-  const [characteristics, setCharacteristics] = React.useState([]);
+const App: React.FC = () => {
+  const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
-    async function fetchData() {
-      // Get the categories
-      const categoriesResponse = await getCategories();
-      const parsedCategories = parseData(categoriesResponse);
-      const cleanedCategories = cleanData(parsedCategories);
-      setCategories(cleanedCategories);
-
-      // Get the tournaments
-      const tournamentsResponse = await getTournaments();
-      const parsedTournaments = parseData(tournamentsResponse);
-      const cleanedTournaments = cleanData(parsedTournaments);
-      setTournaments(cleanedTournaments);
-
-      // Get the team stats
-      const teamStatsResponse = await getTeamStats();
-      const parsedTeamStats = parseData(teamStatsResponse);
-      const cleanedTeamStats = cleanData(parsedTeamStats);
-      setTeamStats(cleanedTeamStats);
-
-      // Get the player stats
-      const playerStatsResponse = await getPlayerStats();
-      const parsedPlayerStats = parseData(playerStatsResponse);
-      const cleanedPlayerStats = cleanData(parsedPlayerStats);
-      setPlayerStats(cleanedPlayerStats);
-
-      // Get the transfers
-      const transfersResponse = await getTransfers();
-      const parsedTransfers = parseData(transfersResponse);
-      const cleanedTransfers = cleanData(parsedTransfers);
-      setTransfers(cleanedTransfers);
-
-      // Get the heat map
-      const heatMapResponse = await getHeatMap();
-      const parsedHeatMap = parseData(heatMapResponse);
-      const cleanedHeatMap = cleanData(parsedHeatMap);
-      setHeatMap(cleanedHeatMap);
-
-      // Get the penalty history
-      const penaltyHistoryResponse = await getPenaltyHistory();
-      const parsedPenaltyHistory = parseData(penaltyHistoryResponse);
-      const cleanedPenaltyHistory = cleanData(parsedPenaltyHistory);
-      setPenaltyHistory(cleanedPenaltyHistory);
-
-      // Get the season stats
-      const seasonStatsResponse = await getSeasonStats();
-      const parsedSeasonStats = parseData(seasonStatsResponse);
-      const cleanedSeasonStats = cleanData(parsedSeasonStats);
-      setSeasonStats(cleanedSeasonStats);
-
-      // Get the national team stats
-      const nationalTeamStatsResponse = await getNationalTeamStats();
-      const parsedNationalTeamStats = parseData(nationalTeamStatsResponse);
-      const cleanedNationalTeamStats = cleanData(parsedNationalTeamStats);
-      setNationalTeamStats(cleanedNationalTeamStats);
-
-      // Get the characteristics
-      const characteristicsResponse = await getCharacteristics();
-      const parsedCharacteristics = parseData(characteristicsResponse);
-      const cleanedCharacteristics = cleanData(parsedCharacteristics);
-      setCharacteristics(cleanedCharacteristics);
-    }
-    fetchData();
+    const fetchDataAndWriteToCsv = async () => {
+      const response = await fetchData();
+      const cleanedData = cleanData(response);
+      const parsedData = parseData(cleanedData);
+      const manipulatedData = manipulateData(parsedData);
+      setData(manipulatedData);
+      writeToCSV(manipulatedData);
+    };
+    fetchDataAndWriteToCsv();
   }, []);
 
   return (
-    <div className="App">
-      // ...
+    <div>
+      {data.map(datum => (
+        <p>{datum.name}</p>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- src/api.ts
+++ src/api.ts
@@ -1,110 +1,150 @@
-import axios from 'axios';
+import axios from "axios";
+import { writeFileSync } from "fs";
 
-import { cleanData, manipulateData, parseData } from './utilities';
-
-export const getData = async (url: string): Promise<any> => {
-  const response = await fetch(url);
-  const data = await response.json();
-  const parsedData = parseData(data);
-  const cleanedData = cleanData(parsedData);
-  const manipulatedData = manipulateData(cleanedData);
-  return manipulatedData;
-}
-const getCategories = async () => {
-  const response = await axios.get('https://api.sofascore.com/api/v1/sport/football/categories');
+const fetchCategories = async () => {
+  const response = await axios.get(
+    "https://api.sofascore.com/api/v1/sport/football/categories"
+  );
   return response.data;
 };
 
-const getTournaments = async (categoryId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/category/${categoryId}/unique-tournaments`);
+const fetchUniqueTournaments = async (categoryId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/category/${categoryId}/unique-tournaments`
+  );
   return response.data;
 };
 
-const getTeamStanding = async (tournamentId: number, seasonId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/unique-tournament/${tournamentId}/season/${seasonId}/standings/total`);
+const fetchTeamStandings = async (
+  uniqueTournamentId: number,
+  seasonId: number
+) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/unique-tournament/${uniqueTournamentId}/season/${seasonId}/standings/total`
+  );
   return response.data;
 };
 
-const getTeamInfo = async (teamId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/team/${teamId}`);
+const fetchTeamInfo = async (teamId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/team/${teamId}`
+  );
   return response.data;
 };
 
-const getTeamTransfers = async (teamId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/team/${teamId}/transfers`);
+const fetchTeamTransfers = async (teamId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/team/${teamId}/transfers`
+  );
   return response.data;
 };
 
-const getTeamStandingsSeasons = async (teamId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/team/${teamId}/standings/seasons`);
+const fetchTeamStandingsSeasons = async (teamId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/team/${teamId}/standings/seasons`
+  );
   return response.data;
 };
 
-const getTeamPlayers = async (teamId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/team/${teamId}/players`);
+const fetchTeamPlayers = async (teamId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/team/${teamId}/players`
+  );
   return response.data;
 };
 
-const getPlayerInfo = async (playerId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}`);
+const fetchPlayerInfo = async (playerId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}`
+  );
   return response.data;
 };
 
-const getPlayerAttributeOverviews = async (playerId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}/attribute-overviews`);
+const fetchPlayerAttributeOverviews = async (playerId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}/attribute-overviews`
+  );
   return response.data;
 };
 
-const getPlayerPenaltyHistory = async (playerId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}/penalty-history`);
+const fetchPlayerPenaltyHistory = async (playerId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}/penalty-history`
+  );
   return response.data;
 };
 
-const getPlayerLastYearSummary = async (playerId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}/last-year-summary`);
+const fetchPlayerLastYearSummary = async (playerId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}/last-year-summary`
+  );
   return response.data;
 };
 
-const getPlayerStatisticsSeasons = async (playerId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}/statistics/seasons`);
+const fetchPlayerStatistics = async (playerId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}/statistics/seasons`
+  );
   return response.data;
 };
 
-const getPlayerNationalTeamStatistics = async (playerId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}/national-team-statistics`);
+const fetchPlayerNationalTeamStatistics = async (playerId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}/national-team-statistics`
+  );
   return response.data;
 };
 
-const getPlayerCharacteristics = async (playerId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}/characteristics`);
+const fetchPlayerCharacteristics = async (playerId: number) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}/characteristics`
+  );
   return response.data;
 };
 
-const getPlayerUniqueTournamentStatistics = async (playerId: number, tournamentId: number, seasonId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}/unique-tournament/${tournamentId}/season/${seasonId}/statistics/overall`);
+const fetchPlayerUniqueTournamentStatistics = async (
+  playerId: number,
+  uniqueTournamentId: number,
+  seasonId: number
+) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}/unique-tournament/${uniqueTournamentId}/season/${seasonId}/statistics/overall`
+  );
   return response.data;
 };
 
-const getPlayerUniqueTournamentHeatmap = async (playerId: number, tournamentId: number, seasonId: number) => {
-  const response = await axios.get(`https://api.sofascore.com/api/v1/player/${playerId}/unique-tournament/${tournamentId}/season/${seasonId}/heatmap/overall`);
+const fetchPlayerUniqueTournamentHeatmap = async (
+  playerId: number,
+  uniqueTournamentId: number,
+  seasonId: number
+) => {
+  const response = await axios.get(
+    `https://api.sofascore.com/api/v1/player/${playerId}/unique-tournament/${uniqueTournamentId}/season/${seasonId}/heatmap/overall`
+  );
   return response.data;
+};
+
+const writeToCSV = (data: any) => {
+  const csvString = data.map((row) => Object.values(row).join(",")).join("\n");
+  writeFileSync("./data.csv", csvString);
 };
 
 export {
-  getCategories,
-  getTournaments,
-  getTeamStanding,
-  getTeamInfo,
-  getTeamTransfers,
-  getTeamStandingsSeasons,
-  getTeamPlayers,
-  getPlayerInfo,
-  getPlayerAttributeOverviews,
-  getPlayerPenaltyHistory,
-  getPlayerLastYearSummary,
-  getPlayerStatisticsSeasons,
-  getPlayerNationalTeamStatistics,
-  getPlayerCharacteristics,
-  getPlayerUniqueTournamentStatistics,
-  getPlayerUniqueTournamentHeatmap
+  fetchCategories,
+  fetchUniqueTournaments,
+  fetchTeamStandings,
+  fetchTeamInfo,
+  fetchTeamTransfers,
+  fetchTeamStandingsSeasons,
+  fetchTeamPlayers,
+  fetchPlayerInfo,
+  fetchPlayerAttributeOverviews,
+  fetchPlayerPenaltyHistory,
+  fetchPlayerLastYearSummary,
+  fetchPlayerStatistics,
+  fetchPlayerNationalTeamStatistics,
+  fetchPlayerCharacteristics,
+  fetchPlayerUniqueTournamentStatistics,
+  fetchPlayerUniqueTournamentHeatmap,
+  writeToCSV,
 };
